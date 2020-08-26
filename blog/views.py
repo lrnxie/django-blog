1daf104156d57007c87e0550f01556f524cc8bac
Lauren Xie <lrnxie@gmail.com>
2020-08-26 16:00:56 -0400

    Initialize blog templates, views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 25, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+    }
+]
+
+
+def home(request):
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    context = {'title': 'About'}
+    return render(request, 'blog/about.html', context)
